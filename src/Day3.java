4c593a44bf6c5e57bf5c6d5a2f7152cb852fcf51
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-10-14 19:06:56 +0300

    alll

---
+++ src/Day3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Day3 {
+
+//Day 3:
+    public static void main(String[] args) {
+        //scanner
+        Scanner read = new Scanner(System.in);
+        //if() else statments.
+        /*
+        System.out.println("Hello, Do you want to start the trip at 6:00am or 7:00am ? (input 6 or 7)");
+        int time = read.nextInt();
+        if(time==6)
+             System.out.println("your trip will start at 6:00am");
+        else System.out.println("your trip will start at 7:00am");
+        */
+System.out.println("welcome, enter your name:");
+String name = read.nextLine();
+
+
+char size='a';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
